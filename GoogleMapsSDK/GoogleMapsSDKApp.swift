55c63a035a1ee292b0e37608e892300520ea7764
ihValery <ihValery@gmail.com>
2021-10-20 12:06:06 +0300

    Initial Commit

---
+++ GoogleMapsSDK/GoogleMapsSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapsSDKApp.swift
+//  GoogleMapsSDK
+//
+//  Created by Валерий Игнатьев on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMapsSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
